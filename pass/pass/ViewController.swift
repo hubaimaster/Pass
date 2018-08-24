265d8cf443cab96b1c35c26b064096ac681e1598
kimchanghwan <hubaimaster@gmail.com>
2018-08-24 22:49:23 +0900

    iOS Creation

---
+++ pass/pass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pass
+//
+//  Created by Chang Hwan Kim on 2018. 8. 24..
+//  Copyright © 2018년 SogangRelease2018. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
