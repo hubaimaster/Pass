9a1e26cda871b3d8858c5d4f0f7342a31ba2e843
kimchanghwan <hubaimaster@gmail.com>
2018-08-25 20:34:56 +0900

    FinalFinal

--- pass/pass/Format.swift
+++ pass/pass/Format.swift
@@ -11,13 +11,15 @@ import Foundation
 class Format {
     
     static func timeAgoSince(from: Int)->String{
-        let date = Date(timeIntervalSince1970: TimeInterval(from))
+        let date = Date(timeIntervalSince1970: TimeInterval(from) / 1000.0)
         return timeAgoSince(date: date)
     }
     
     static func timeAgoSince(date: Date) -> String {
         let calendar = Calendar.current
         let now = Date()
+        print("nowe:\(now.timeIntervalSince1970)")
+        print("date:\(date.timeIntervalSince1970)")
         let unitFlags: NSCalendar.Unit = [.second, .minute, .hour, .day, .weekOfYear, .month, .year]
         let components = (calendar as NSCalendar).components(unitFlags, from: date, to: now, options: [])
         
