635779ec69448760aae7bab881c4eb3cbc247304
SpiroSpica <hath1997@gmail.com>
2018-08-25 12:38:09 +0900

    Update market.controller.js

--- pass_server/api/market/market.controller.js
+++ pass_server/api/market/market.controller.js
@@ -6,10 +6,42 @@ var util = require('../../utils/util')
 
 exports.register = (req,res) => {
   res.send("Market Register")
+  var payload = req.body
+
+  var onSuccess = (pass) => {
+    console.log(pass)
+    res.status(200).json(util.successTrue(pass))
+  }
+
+  var onError = (error) => {
+    console.error(error)
+    res.status(400).json(util.successFalse(error))
+  }
+
+  market.createMarket(payload)
+  .then(onSuccess)
+  .catch(onError)
+  
 }
 
 exports.marketInfo = (req,res) => {
   res.send("Market Info")
+  
+  var id = req.param.marketId
+
+  var onSuccess = (pass) => {
+    console.log(pass)
+    res.status(200).json(util.successTrue(pass))
+  }
+
+  var onError = (error) => {
+    console.error(error)
+    res.status(400).json(util.successFalse(error))
+  }
+
+  market.findOneById(id)
+  .then(onSuccess)
+  .catch(onError)
 }
 
 exports.marketList = (req,res) => {
