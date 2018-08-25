1024e526628d89109824806f9f1b592dc9a9a159
Se Jin Lee <sjlee2016@sogang.ac.kr>
2018-08-25 12:30:31 +0900

    Update user.controller.js

--- pass_server/api/user/user.controller.js
+++ pass_server/api/user/user.controller.js
@@ -3,11 +3,40 @@
 var user = require('../../models/User')
 var util = require('../../utils/util')
 
-
+// POST
+// register new user
 exports.register = (req,res) => {
-  res.send("User Register")
-}
+  var payload = req.body
 
+  var onSuccess = (user) => {
+   console.log(user)
+   res.status(200).json(util.successTrue(user))
+  }
+
+  var onError = (error) => {
+        console.error(error)
+        res.status(400).json(util.successFalse(error))
+  }
+  user.createUser(payload)
+  .then(onSuccess)
+  .catch(onError)
+
+
+}
+// GET
+// user login
 exports.login = (req,res) => {
-  res.send("User login")
+  var pqyload = req.param
+
+  var onSuccess = (user) => {
+      console.log(pass)
+      res.status(200).json(util.successTrue(user))
+  }
+  var onError = (error) => {
+      console.error(error)
+      res.status(400).json(util.successFalse(error))
+ }
+ user.findOneById(payload)
+ .then(onSuccess)
+ .catch(onError)
 }
